b1a37b7a7dd06c1f0dc79ed36e53217e6755c0a8
snajder <rene.snajder@gmail.com>
2022-06-07 17:10:05 +0200

    Working on results for paper revision

--- benchmark_pycometh/diffmet/asm_results.py
+++ benchmark_pycometh/diffmet/asm_results.py
@@ -4,11 +4,12 @@ from meth5 import MetH5File
 
 
 class Results:
-    def __init__(self, reference_cpgs, m5_path, include_chroms=None):
+    def __init__(self, reference_cpgs, m5_path, include_chroms=None, read_group_key="haplotype"):
         self.reference_cpgs = reference_cpgs
         self.segments = {}
         self.m5_path = m5_path
         self.include_chroms = include_chroms
+        self.read_group_key = read_group_key
     
     def annotate_cpgs(self, segments):
         segments["CpGs"] = segments.apply(
@@ -16,13 +17,19 @@ class Results:
         )
         return segments
     
-    def compute_diffmet(self, mf, chrom, start, end):
-        agg = mf[chrom].get_values_in_range(start, end).get_llr_site_readgroup_rate("haplotype")
+    def compute_diffmet(self, mf, chrom, start, end, min_calls=2):
+        agg = mf[chrom].get_values_in_range(start, end).get_llr_site_readgroup_rate(self.read_group_key)
+        if len(agg) < 2:
+            return np.nan
+        if len(agg[0][1]) < min_calls or len(agg[1][1]) < min_calls:
+            # Too few calls:
+            return np.nan
         try:
             diffmet = np.abs(np.nanmean(agg[0][0]) - np.nanmean(agg[1][0]))
             return diffmet
         except:
             print("Can't compute difference for ", chrom, start, end)
+            return np.nan
     
     def load_segments(self, key, file, caller):
         print(f"Loading {key}")
@@ -40,6 +47,7 @@ class Results:
                 names=["chrom", "start", "end", "nC.valid", "nC", "diffmet_mcp", "cov", "adj_pvalue"],
                 dtype={"chrom": str},
             )
+            data = data.loc[data["adj_pvalue"] < 0.05].copy()
         
         if self.include_chroms is not None:
             print("Filtering chromosomes")
@@ -47,10 +55,11 @@ class Results:
         
         print(f"Annotating CpGs for {key}")
         data = self.annotate_cpgs(data)
-        print(f"Loading diffmet for {key}")
+        print(f"Loading diffmet for {key} ({data.shape[0]} segments)")
         with MetH5File(self.m5_path, "r") as mf:
             data["diffmet"] = data.apply(
                 lambda row: self.compute_diffmet(mf, row["chrom"], row["start"], row["end"]), axis=1
             )
-        print(f"Finished loading {key}")
+            data = data.loc[data["diffmet"].map(lambda x: not np.isnan(x))]
+        print(f"Finished loading {key} ({data.shape[0]} segments)")
         self.segments[key] = data
